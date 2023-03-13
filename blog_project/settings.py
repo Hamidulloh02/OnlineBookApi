bbd9a139efc8c9b13e95443bcbb18fb9fdf4e8bd
Hamidjonn <hamidullonematullayev22@gmail.com>
2023-03-13 16:43:57 +0500

    change allower_host

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -16,8 +16,9 @@ SECRET_KEY = 'django-insecure-cjc*6@5weex4o9yqyi=e^u2k^r+sns@_oga5_kazsc(wwk1&@0
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = [".pythonanywhere.com",'127.0.0.1','*']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://online-bookapi.up.railway.app/']
+
 
 DEBUG = False
 
